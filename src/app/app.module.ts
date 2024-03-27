2b998b7c36dcb364bd54c8fd80c28f11e894e1db
Andres Mendoza <andresfelipemendozayepes@gmail.com>
2024-03-26 20:51:16 -0500

    creacion perfil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrarUsuarioComponent } from './registrar-usuario/registrar-usuario.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PerfilComponent } from './perfil/perfil.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrarUsuarioComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/perfil/perfil.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil',
+  templateUrl: './perfil.component.html',
+  styleUrls: ['./perfil.component.css']
+})
+export class PerfilComponent {
+
+  
+}
+// perfil.model.ts
+
+export interface Perfil {
+  id: number;
+  nombre: string;
+  apellido: string;
+  edad: number;
+  carrera: string;
+  intereses: string[];
+  // Otras propiedades según sea necesario
+}
