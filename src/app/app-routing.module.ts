a1b76287bb9199466f04e2fd99a40a58bb59bd53
Andres Mendoza <andresfelipemendozayepes@gmail.com>
2024-04-20 09:33:49 -0500

    Routing implements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrarUsuarioComponent } from './registrar-usuario/registrar-usuario.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'inicio', component: InicioComponent},
+  { path:'login', component: LoginComponent},
+  { path:'registrarUsuario', component: RegistrarUsuarioComponent},
+  { path:'perfil', component: PerfilComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
